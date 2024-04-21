dd1f1391b1d2d4d7c8f9fa3b752b9fdca5a5dde8
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-04-21 23:41:52 +0300

    init TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import userData from '../userData.json'
 import friends from '../friends.json'
+import transactions from '../transactions.json'
 import Profile from "./Profile/Profile";
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory'
 
 
 
@@ -10,6 +12,7 @@ export default function App() {
     <>
       <Profile name={userData.username} tag={userData.tag} location={userData.location} image={userData.avatar} stats={userData.stats} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   )
 }
